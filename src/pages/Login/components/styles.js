141301e00b4b1fad99e68c440a8aa839613a2f71
Lucas Maia <lucas.codemax@gmail.com>
2023-07-30 13:24:20 -0300

    up

--- src/pages/Login/components/styles.js
+++ src/pages/Login/components/styles.js
@@ -29,6 +29,19 @@ export const Box = styled.View`
 	background: ${props => props.theme[props.background] || props.background || '#fff'};
 `
 
+export const SocialArea = styled.View`
+	flex-wrap: ${(props) => props.wrap || 'nowrap'};
+	flex-direction: ${(props) => (props.row ? 'row' : 'column')};
+	justify-content: ${(props) => props.justify || 'flex-start'};
+	align-items: ${(props) => props.align || 'flex-start'};
+
+	width: ${(props) => props.width || '100%'};
+	max-width: ${(props) => props.width || '100%'};
+	min-width: ${(props) => props.width || '100%'};
+
+	margin-top: 20px;
+`
+
 export const Title = styled(TitlePaper)`
 	color: ${(props) => props.theme[props.color || 'secondary']};
 	font-size: ${(props) => (props.small ? '22px' : props.big ? '50px' : '30px')};
@@ -45,24 +58,80 @@ export const BtnLogin = styled(ButtonPaper).attrs(props => ({
 		fontFamily: 'Quicksand_700Bold',
 		textTransform: 'none',
 		letterSpacing: -1,
-		fontSize: 18
+		fontSize: 18,
+		marginBottom: 12
 	}
-}))``
+}))`
+	margin-top: 30px;
+`
+
+export const BtnCadastro = styled(ButtonPaper).attrs(props => ({
+	mode: props.mode || 'contained',
+	width: props.block ? '100%' : 'auto',
+	color: 'transparent',
+	labelStyle: {
+		fontFamily: 'Quicksand_700Bold',
+		textTransform: 'none',
+		letterSpacing: -0.8,
+		fontSize: 19,
+		marginLeft: 0,
+		marginRight: 0,
+		marginTop: 0,
+		marginBottom: 0,
+		padding: 0,
+		color: '#f85f6a'
+	}
+}))`
+	padding: 0;
+	elevation: 0;
+`
+
+export const BtnRecuperarSenha = styled(ButtonPaper).attrs(props => ({
+	mode: props.mode || 'contained',
+	width: props.block ? '100%' : 'auto',
+	color: 'transparent',
+	labelStyle: {
+		fontFamily: 'Quicksand_600SemiBold',
+		textTransform: 'none',
+		letterSpacing: -1,
+		fontSize: 17,
+		marginLeft: 0,
+		marginRight: 0,
+		marginTop: 0,
+		marginBottom: 0,
+		padding: 0,
+		color: '#989eb1'
+	}
+}))`
+	padding: 0;
+	elevation: 0;
+`
 
 export const Logo = styled.Image`
 	height: 40px;
 	width: 190px;
 	margin-bottom: 40px;
+	margin-top: ${(props) => props.top || 0};
 `
 export const TextDescription = styled.Text`
 	color: #989eb1;
-	font-size: 16px;
+	font-size: 18px;
 	font-family: 'Quicksand_500Medium';
 `
 
+export const TextHintSocial = styled.Text`
+	color: #989eb1;
+	font-size: 14px;
+	text-align: center;
+	width: 100%;
+	margin-top: 15px;
+	margin-bottom: 10px;
+`
+
 export const TextInput = styled(TextInputPaper).attrs({
-	outlineColor: '#c00',
+	outlineColor: '#ebebeb',
 	selectionColor: '#000',
+	activeOutlineColor: '#e0e0e0',
 	theme: {
 		colors: {
 			background: '#f0f0f0'
@@ -77,4 +146,5 @@ export const Label = styled.Text`
 	font-size: 16px;
 	color: #f85f6a;
 	font-weight: 600;
+	margin-top: ${(props) => props.top || '20px'};
 `
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {Image, Text} from "react-native"
-import {Title, Box, Logo, TextDescription, BtnLogin, TextInput, Label} from './components/styles'
+import {Title, Box, Logo, TextDescription, BtnLogin, TextInput, Label, TextHintSocial, SocialArea, BtnCadastro, BtnRecuperarSenha} from './components/styles'
 import logo from '../../assets/images/logo.png'
 
 const Index = () => {
@@ -8,17 +8,24 @@ const Index = () => {
 
 	return (
 		<>
-			<Box hasPadding>
-				<Logo source={logo} />
+			<Box justify="center" hasPadding>
+				<Logo top="30px" source={logo} />
+
 				<Title>Entrar</Title>
 				<TextDescription>Olá! Bom ver você de novo.</TextDescription>
 
-				<Box column>
-					<Label>Endereço de e-mail</Label>
-					<TextInput mode="outlined" autoFocus={false} />
-				</Box>
+				<Label top="30px">Endereço de e-mail</Label>
+				<TextInput mode="outlined" autoFocus={false} />
+
+				<Label>Senha</Label>
+				<TextInput mode="outlined" secureTextEntry autoFocus={false} />
 
 				<BtnLogin block>Entrar</BtnLogin>
+
+				<SocialArea row justify="space-between">
+					<BtnRecuperarSenha>Esqueceu sua senha?</BtnRecuperarSenha>
+					<BtnCadastro>Cadastre-se</BtnCadastro>
+				</SocialArea>
 			</Box>
 		</>
 	)
